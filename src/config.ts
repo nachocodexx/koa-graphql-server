d1886e2a72011ab407a12753bf18acf224a00008
Nacho Castillo <4811095288ic@gmail.com>
2018-12-28 23:21:49 -0600

    corrected an extra letter in mongodb uri [fixed]

--- src/config.ts
+++ src/config.ts
@@ -5,5 +5,4 @@ export const PORT: number = parseInt(process.env.PORT) || 7000,
     DBNANME: string = 'nameless',
     //You can modify this constants with your mongo user credentials.  
     mongoUser: string = 'admin',
-    mongoPass: string = '123456',
-    MONGODB_URI: string = process.env.MONGODB_URI || `mongodb://${mongoUser}:${mongoPass}6@localhost:27017/${DBNANME}?authSource=admin` 
+    mongoPass: string = '123456', MONGODB_URI: string = process.env.MONGODB_URI || `mongodb://${mongoUser}:${mongoPass}@localhost:27017/${DBNANME}?authSource=admin` 
