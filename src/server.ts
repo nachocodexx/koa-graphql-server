0ebf01738e689a3d47aebe18aa25bf6b611c0da6
Nacho Castillo <4811095288ic@gmail.com>
2019-04-09 13:35:18 -0500

    KoaServer variable name [refactored]

--- src/server.ts
+++ src/server.ts
@@ -1,54 +1,48 @@
-import * as Koa from 'koa'
+import * as Koa from "koa";
 import KoaBodyParser = require("koa-bodyparser");
-import { PORT } from './config'
-import { apolloServer } from './graphQL/';
-const KoaCors = require('@koa/cors');
-
+import { PORT } from "./config";
+import { apolloServer } from "./graphQL/";
+const KoaCors = require("@koa/cors");
 
 //Logger
-const logger = require('koa-logger')
-
+const logger = require("koa-logger");
 
 class KoaServer extends Koa {
-    private port: number
-    readonly app: Koa
+  private port: number;
+  readonly app: Koa;
 
-    constructor(port: number) {
-        super()
-        this.port = port
-        // CONFIG KOA APP  / MIDDLEWARES
-        this.setConfig()
-        // INIT KOA ROUTER 
-        this.initRouter()
-    }
+  constructor(port: number) {
+    super();
+    this.port = port;
+    // CONFIG KOA APP  / MIDDLEWARES
+    this.setConfig();
+    // INIT KOA ROUTER
+    this.initRouter();
+  }
 
-    //SET PLUGINS (MIDDLEWARES)
-    private setConfig(): void {
-        //BODY PARSER MIDDLEWARE 
-        this.use(KoaBodyParser())
-        //cors
-        this.use(KoaCors())
-        //logger
-        this.use(logger())
-    }
+  //SET PLUGINS (MIDDLEWARES)
+  private setConfig(): void {
+    //BODY PARSER MIDDLEWARE
+    this.use(KoaBodyParser());
+    //cors
+    this.use(KoaCors());
+    //logger
+    this.use(logger());
+  }
 
-    //INIT ROUTER 
-    private initRouter(): void {
-    }
+  //INIT ROUTER
+  private initRouter(): void {}
 
-    //GET APP
-    public getApp(): Koa {
-        return this.app
-    }
+  //GET APP
+  public getApp(): Koa {
+    return this.app;
+  }
 
-    //RUN APP 
-    public run(): void {
-        this.listen(this.port, '0.0.0.0')
-    }
-
+  //RUN APP
+  public run(): void {
+    this.listen(this.port, "0.0.0.0");
+  }
 }
 
-
-
-export const App: KoaServer = new KoaServer(PORT)
-apolloServer.applyMiddleware({ app: App })
+export const app: KoaServer = new KoaServer(PORT);
+apolloServer.applyMiddleware({ app });
